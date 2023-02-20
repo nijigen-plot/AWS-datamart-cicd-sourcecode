7a50d6a915d920c66d8eae837c62e57051ea12f6
Tomoya Shimoma <s.tomoya@tunecore.co.jp>
2023-02-21 03:03:43 +0900

    zipファイル解凍後1つ下の階層にlambda_function.pyが配置されるよう変更

--- aws/script/create_zip_files.sh
+++ aws/script/create_zip_files.sh
@@ -6,7 +6,7 @@ mkdir -p ${WORKDIR}
 cd aws/lambda/src
 for dir in `ls -d */`
 do
-    zip -r ${WORKDIR}`basename ${dir}`.zip `basename ${dir}`
+    zip -r -j ${WORKDIR}`basename ${dir}`.zip `ls ${dir}/* | grep lambda_function.py`
 done
 
 aws s3 sync --exact-timestamps --delete ${WORKDIR} s3://quark-cicd-test/lambda/src/
